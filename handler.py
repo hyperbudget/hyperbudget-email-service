fb244c9d84e41bac913b883890f5f93cc3a4d251
Errietta Kostala <errietta@errietta.me>
2018-11-12 20:50:44 +0000

    add hello world

--- handler.py
+++ handler.py
@@ -12,13 +12,6 @@ def send_email(event, context):
         "body": json.dumps(body)
     }
 
-    return response
+    print("hello world, event called")
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    return response
